3367fdd19c83790c3465d9691f549e1cd35a60ef
phil002109 <vermontphil.paguiligan@gmail.com>
2023-08-22 19:27:30 +0800

    Added function overseeAuction

--- src/HundredDollarAuction.sol
+++ src/HundredDollarAuction.sol
@@ -9,7 +9,7 @@ import {USDTest} from "./USDTest.sol";
  * @author Vermont Phil Paguiligan
  *
  * Terms:
- * Auctioneer - the owner of this contract
+ * Auctioneer - entity that has the authority to end the auction
  * Bidder - the address of the user who joins the bid
  * Winning Bid - the highest standing bid amount when the auction ends
  * Losing Bid - the lowest standing bid amount when the auction ends
@@ -24,17 +24,20 @@ import {USDTest} from "./USDTest.sol";
  * 2. The other bidder should forfeit or the auctioneer ends the auction with your winning
  *    bid for you to win.
  * 3. When the auction ends, the winning and losing bid will be collected by this contract.
- *    And the price of $100 USDTest will be given to the highest bidder.
- * 4. When either of the bid reaches 100 USDTest, the auctioneer can end the
+ *    And the price of $100 will be given to the highest bidder.
+ * 4. When either of the bid reaches $100, the auctioneer can end the
  *    auction anytime.
  */
 contract HundredDollarAuction is Ownable {
     error HundredDollarAuction__TransferFailed();
     error HundredDollarAuction__BidCollectionFailed();
-    error HundredDollarAuction__AuctionOccupied();
+    error HundredDollarAuction__BiddersOccupied();
+    error HundredDollarAuction__AuctioneerOccupied();
     error HundredDollarAuction__AmountDidNotOutbid();
     error HundredDollarAuction__NotABidder();
     error HundredDollarAuction__LessThanTwoBidders();
+    error HundredDollarAuction__TheSameBidderNotAllowed();
+    error HundredDollarAuction__BidderCantOverseeAuction();
 
     enum Status { WAITING, ACTIVE, CAN_COLLECT, ENDED }
 
@@ -46,11 +49,12 @@ contract HundredDollarAuction is Ownable {
     // instead of finding bidder and opponent bidder everytime.
     mapping(address bidder => address opponentBidder) private s_opponentBidder;
 
+    address private s_auctioneer;
     address private s_firstBidder;
     address private s_secondBidder;
     address private s_winningBidder;
     USDTest private immutable i_usdt;
-    Status private status = Status.WAITING;
+    Status private s_status = Status.WAITING;
 
     constructor(USDTest usdt, address firstBidder) {
         i_usdt = usdt;
@@ -74,15 +78,18 @@ contract HundredDollarAuction is Ownable {
 
     function joinAuction(uint256 bidAmount) public {
         if (s_secondBidder != address(0)) {
-            revert HundredDollarAuction__AuctionOccupied();
+            revert HundredDollarAuction__BiddersOccupied();
         }
         if (bidAmount <= INITIAL_BID_AMOUNT) {
             revert HundredDollarAuction__AmountDidNotOutbid();
         }
+        if (s_secondBidder == msg.sender) {
+            revert HundredDollarAuction__TheSameBidderNotAllowed();
+        }
 
         s_secondBidder = msg.sender;
         s_bidAmounts[s_secondBidder] = INITIAL_BID_AMOUNT;
-        status = Status.ACTIVE;
+        s_status = Status.ACTIVE;
 
         s_opponentBidder[s_firstBidder] = s_secondBidder;
         s_opponentBidder[s_secondBidder] = s_firstBidder;
@@ -111,6 +118,34 @@ contract HundredDollarAuction is Ownable {
         }
     }
 
+    /**
+     * We will need someone to be an Auctioneer.
+     * The role of the auctioneer is to end the auction when either of
+     * the bid gets $100 of more.
+     * Auctioneer will get a 10% commission based on the auction profit.
+     * 
+     * Example:
+     * auctionPrice = $100
+     * winningBid = $110
+     * losingBid = $90
+     * 
+     * collectedBids = winningBid + losingBid // $110 + $90 = $200
+     * 
+     * profit = collectedBids - auctionPrice // $200 - $100 = $100
+     * 
+     * commission = profit * 10% // $100 * 10% = $10
+     */
+    function overseeAuction() public {
+        if (s_auctioneer != address(0)) {
+            revert HundredDollarAuction__AuctioneerOccupied();
+        }
+        if (msg.sender == s_firstBidder || msg.sender == s_secondBidder) {
+            revert HundredDollarAuction__BidderCantOverseeAuction();
+        }
+
+        s_auctioneer = msg.sender;
+    }
+
     function endAuction() public onlyOwner {}
 
     function _endAuction(address winner) private {
@@ -124,14 +159,18 @@ contract HundredDollarAuction is Ownable {
             revert HundredDollarAuction__BidCollectionFailed();
         }
 
-        status = Status.ENDED;
+        s_status = Status.ENDED;
     }
 
     function _updateWinningBidder() private {
         s_winningBidder = msg.sender;
 
         if (s_bidAmounts[s_winningBidder] >= AUCTION_PRICE) {
-            status = Status.CAN_COLLECT;
+            s_status = Status.CAN_COLLECT;
         }
+    }
+
+    function _getStatus() private view returns (Status) {
+        return s_status;
     }
 }
