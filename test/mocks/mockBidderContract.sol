895d7448aa3aea1cba9434c0fe8adce9870245c6
phil002109 <vermontphil.paguiligan@gmail.com>
2023-09-20 14:14:48 +0800

    Added functions to mockBidderContract

--- test/mocks/mockBidderContract.sol
+++ test/mocks/mockBidderContract.sol
@@ -2,4 +2,28 @@
 
 pragma solidity 0.8.18;
 
-contract Bidder {}
+interface HundredDollarAuction {
+    function joinAuction(uint256 amountToBid) external;
+}
+
+interface USDTFaucet {
+    function requestUSDT() external;
+}
+
+interface USDT {
+    function approve(address spender, uint256 amount) external;
+}
+
+contract Bidder {
+    function joinAuction(HundredDollarAuction auction, uint256 amountToBid) external {
+        auction.joinAuction(amountToBid);
+    }
+
+    function requestUSDT(USDTFaucet faucet) external {
+        faucet.requestUSDT();
+    }
+
+    function approve(USDT usdt, address spender, uint256 amount) external {
+        usdt.approve(spender, amount);
+    }
+}
